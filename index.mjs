cf11cce072d7f3b666e15316d8648f83bb8813a2
Rien Maertens <rien.maertens@posteo.be>
2023-03-02 13:06:32 +0100

    Update for Dolos v2.1.0

--- index.mjs
+++ index.mjs
@@ -10,9 +10,8 @@ const files = [
 const dolos = new Dolos();
 const report = await dolos.analyzePaths(files);
 
-for (const scoredPair of report.scoredPairs) {
- const pair = scoredPair.pair;
- for (const fragment of pair.fragments()) {
+for (const pair of report.allPairs()) {
+ for (const fragment of pair.buildFragments()) {
     const left = fragment.leftSelection;
     const right = fragment.rightSelection;
     console.log(`${pair.leftFile.path}:{${left.startRow},${left.startCol} -> ${left.endRow},${left.endCol}} matches with ${pair.rightFile.path}:{${right.startRow},${right.startCol} -> ${right.endRow},${right.endCol}}`);
